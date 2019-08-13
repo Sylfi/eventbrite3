da8aa3030575871f3f9b73d71451da85c9540c70
Syle <sylvain.firer@gmail.com>
2019-08-13 19:58:32 +0200

    added authenticate

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  get 'users/index'
-  get 'users/show'
+  # get 'users/index'
+  # get 'users/show'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get 'events/index'
+  # get 'events/index'
   resources :events
   root "events#index"
   resources :users
